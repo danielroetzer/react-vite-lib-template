c89a5f4b814476e27e3af7775b6858f43619c8e4
Daniel Rötzer <52409858+danielroetzer@users.noreply.github.com>
2024-07-08 10:55:20 +0200

    feat: add tree shakeable css module support

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,51 @@
 /// <reference types="vite/client" />
 /// <reference types="vitest" />
 import { defineConfig } from 'vite'
-import { resolve } from 'node:path'
+import { extname, relative, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { globSync } from 'glob'
 import react from '@vitejs/plugin-react-swc'
 import dts from 'vite-plugin-dts'
+import { libInjectCss } from 'vite-plugin-lib-inject-css'
 
-const entryFile = 'lib/index.ts'
+const storiesPattern = '**/*.stories.{ts,tsx}'
+const testsPattern = '**/*.test.{ts,tsx}'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    libInjectCss(),
     dts({
       include: ['lib'],
-      exclude: ['**/*.stories.ts*', '**/*.test.ts*'],
+      exclude: [storiesPattern, testsPattern],
     }),
   ],
   build: {
     lib: {
-      entry: resolve(__dirname, entryFile),
+      entry: resolve(__dirname, 'lib/index.ts'),
       formats: ['es'],
     },
     rollupOptions: {
+      // This is the recommended way to bundle with keeping the original file structure.
+      // The `preserveModules` option would be the alternative, but it breaks the css modules.
+      // See: https://rollupjs.org/configuration-options/#input
+      input: Object.fromEntries(
+        globSync('lib/**/*.{ts,tsx}', {
+          ignore: [storiesPattern, testsPattern, '**/*.d.ts'],
+        }).map((file) => [
+          // This removes `lib/` as well as the file extension from each file,
+          // so e.g. lib/src/components/button.tsx becomes src/components/button
+          relative('lib', file.slice(0, file.length - extname(file).length)),
+          // This expands the relative paths to absolute paths, so e.g.
+          // lib/src/components/button becomes .../project/lib/src/components/button.tsx
+          fileURLToPath(new URL(file, import.meta.url)),
+        ]),
+      ),
       external: ['react', 'react-dom', 'react/jsx-runtime'],
       output: {
         entryFileNames: '[name].js',
         assetFileNames: 'assets/[name][extname]',
-        preserveModules: true,
-        preserveModulesRoot: 'src',
       },
     },
   },
